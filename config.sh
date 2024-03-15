72969e931fa1fc40f31118acfd757aeb0dd8d399
HitBlast <hitblastlive@gmail.com>
2024-03-15 15:14:19 +0600

    added auto-configuration bash script

---
+++ config.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+DOTFILES=(.gitignore .hushlogin .zprofile .zshrc .p10k.zsh)
+
+for dotfile in $(echo ${DOTFILES[*]});
+do
+    cp ~/Developer/dotfiles/$(echo $dotfile) ~/$(echo $dotfile)
+done
