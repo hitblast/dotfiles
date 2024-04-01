50e52ba6dd25e2de457974b7ea8196be20fde172
HitBlast <hitblastlive@gmail.com>
2024-04-01 11:34:05 +0600

    integrate both scripts into one file

--- setup.sh
+++ setup.sh
@@ -62,8 +62,32 @@ git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $HO
 # Run macos_cfg.sh script to configure macOS settings.
 # This runs only if the system is darwin-based and the shell is interactive.
 if [[ "$OSTYPE" == "darwin"* && -t 0 ]]; then
-    echo "Configuring macOS settings..."
-    source macos_cfg.sh
+    # Ask for input if user wants to restore or install.
+    echo "Do you want to restore or install? (r/i)"
+    read -k 1 response
+
+    if [[ $response == "r" ]]; then
+        echo "\nDeleting custom values..."
+
+        # Reset autohide dock.
+        defaults delete com.apple.dock "autohide" && killall Dock
+
+        # Restore defaults for opening blank files in TextEdit and other apps.
+        defaults delete -g NSShowAppCentricOpenPanelInsteadOfUntitledFile
+
+    elif [[ $response == "i" ]]; then
+        echo "\nWriting defaults..."
+
+        # Enable autohide dock.
+        defaults write com.apple.dock "autohide" -bool "true" && killall Dock
+
+        # Write defaults for opening blank files in TextEdit and other apps.
+        defaults write -g NSShowAppCentricOpenPanelInsteadOfUntitledFile -bool false
+
+    else 
+        echo "\nInvalid input. Exiting..."
+        exit 1
+    fi
 else
     echo "Skipping macOS configuration script..."
 fi
