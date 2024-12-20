1dcf6c22ec3b4b8f8ea46e961d120774ffc370b4
HitBlast <hitblastlive@gmail.com>
2024-12-21 00:59:16 +0600

    added greeter

---
+++ .config/nvim/lua/hitblast/plugins/alpha.lua
@@ -0,0 +1,41 @@
+return {
+  "goolord/alpha-nvim",
+  event = "VimEnter",
+  config = function()
+    local alpha = require("alpha")
+    local dashboard = require("alpha.themes.dashboard")
+
+    -- header for dashboard
+    dashboard.section.header.val = {
+      "                              ",
+      "                              ",
+      "                              ",
+      "                              ",
+      "    ▄   ████▄ ▄█ ██▄   ▄████  ",
+      "     █  █   █ ██ █  █  █▀   ▀ ",
+      "█     █ █   █ ██ █   █ █▀▀    ", 
+      " █    █ ▀████ ▐█ █  █  █      ", 
+      "  █  █         ▐ ███▀   █     ", 
+      "   █▐                    ▀    ", 
+      "   ▐                          ",
+      "                              ",
+      "                              ",
+      "                              ",
+    }
+
+    -- set menu
+    dashboard.section.buttons.val = {
+      dashboard.button("e", "New file", "<cmd>ene<CR>"),
+      dashboard.button("SPACE ee", "Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
+      dashboard.button("SPACE ff", "Find file", "<cmd>Telescope find_files<CR>"),
+      dashboard.button("SPACE fs", "Find string", "<cmd>Telescope live_grep<CR>"),
+      dashboard.button("q", "Quit Neovim", "<cmd>qa<CR>"),
+    }
+
+    -- send config to alpha
+    alpha.setup(dashboard.opts)
+
+    -- disable folding on alpha buffer
+    vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
+  end,
+}
