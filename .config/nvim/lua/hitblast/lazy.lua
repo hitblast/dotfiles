e895ded3f4579239f64f89b6669a0afe8efc9fc0
HitBlast <hitblastlive@gmail.com>
2024-12-21 12:58:39 +0600

    added new plugins

--- .config/nvim/lua/hitblast/lazy.lua
+++ .config/nvim/lua/hitblast/lazy.lua
@@ -11,4 +11,8 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("hitblast.plugins")
+require("lazy").setup("hitblast.plugins", {
+  change_detection = {
+    notify = false,
+  },
+})
---
+++ .config/nvim/lua/hitblast/plugins/bufferline.lua
@@ -0,0 +1,14 @@
+return {
+  "akinsho/bufferline.nvim",
+  version = "*",
+  opts = {
+    options = {
+      mode = "tabs",
+      separator_style = "thick",
+      show_buffer_icons = false,
+      indicator = {
+        style = "underline",
+      },
+    },
+  },
+}
