7f003eac680d5590ca22ff30428adf85f04647a6
HitBlast <hitblastlive@gmail.com>
2024-12-25 23:49:59 +0600

    added trackpad, mission control and other new settings

--- macos-mods.sh
+++ macos-mods.sh
@@ -13,33 +13,43 @@ if [ "$user_input" == "ch" ]; then
     defaults write com.apple.finder "FXRemoveOldTrashItems" -bool "true"
     killall Finder
 
-    # custom dock
+    # Trackpad modifications
+    defaults write com.apple.AppleMultitouchTrackpad "FirstClickThreshold" -int "0"
+    defaults write com.apple.AppleMultitouchTrackpad "TrackpadThreeFingerDrag" -bool "true"
+
+    # Custom dock modifications
     defaults write com.apple.dock "tilesize" -int "46"
     defaults write com.apple.dock "autohide" -bool "true"
     defaults write com.apple.dock "orientation" -string "left" && killall Dock
     defaults write com.apple.dock autohide-delay -float 0
     defaults write com.apple.dock autohide-time-modifier -float 0.15
+    defaults write com.apple.dock "expose-group-apps" -bool "true"
     killall Dock
 
 elif [ "$user_input" == "re" ]; then
-    # revert menu bar clock modifications
+    # Revert menu bar clock modifications
     defaults delete com.apple.menuextra.clock "FlashDateSeparators"
     defaults delete com.apple.menuextra.clock "DateFormat"
     killall SystemUIServer
 
-    # revert Finder modifications
+    # Revert Finder modifications
     defaults delete com.apple.finder "AppleShowAllFiles"
     defaults delete com.apple.finder "CreateDesktop"
     defaults delete com.apple.finder "ShowPathbar"
     defaults delete com.apple.finder "FXRemoveOldTrashItems"
     killall Finder
 
-    # revert custom dock settings
+    # Revert trackpad modifications
+    defaults delete com.apple.AppleMultitouchTrackpad "FirstClickThreshold"
+    defaults delete com.apple.AppleMultitouchTrackpad "TrackpadThreeFingerDrag"
+
+    # Revert custom dock modifications
     defaults delete com.apple.dock "tilesize"
     defaults delete com.apple.dock "autohide"
     defaults delete com.apple.dock "orientation"
     defaults delete com.apple.dock autohide-delay
     defaults delete com.apple.dock autohide-time-modifier
+    defaults delete com.apple.dock "expose-group-apps"
     killall Dock
 
 else
