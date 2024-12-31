3104763b58bf60d942237f3ffdee92f8321e3c58
HitBlast <hitblastlive@gmail.com>
2025-01-01 00:16:56 +0600

    updated extension configurations

--- .config/nvim/lua/hitblast/plugins/autopairs.lua
+++ .config/nvim/lua/hitblast/plugins/autopairs.lua
@@ -14,6 +14,7 @@ return {
       }
     })
 
+    -- setup event binding
     local cmp_autopairs = require("nvim-autopairs.completion.cmp")
     local cmp = require("cmp")
     cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
