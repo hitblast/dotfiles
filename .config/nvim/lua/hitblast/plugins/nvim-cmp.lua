853ac59b82e5023655dd45a60317f32ba634ec71
HitBlast <hitblastlive@gmail.com>
2024-12-31 23:28:23 +0600

    added lspconfig for lua and rust

--- .config/nvim/lua/hitblast/plugins/nvim-cmp.lua
+++ .config/nvim/lua/hitblast/plugins/nvim-cmp.lua
@@ -2,7 +2,7 @@ return {
   "hrsh7th/nvim-cmp",
   event = "InsertEnter",
   dependencies = {
-    "hrsh7th/cmp-buffer", 
+    "hrsh7th/cmp-buffer",
     "hrsh7th/cmp-path",
     "onsails/lspkind.nvim",
   },
@@ -24,6 +24,7 @@ return {
         ["<CR>"] = cmp.mapping.confirm({ select = false }),
       }),
       sources = cmp.config.sources({
+        { name = "nvim_lsp" },
         { name = "buffer" },
         { name = "path" },
       }),
