74efbbc5364388a9754f2705545a5a26bc63f7f8
HitBlast <hitblastlive@gmail.com>
2025-01-01 10:48:32 +0600

    added back toggleterm, might need keybind adjustments

---
+++ .config/nvim/lua/hitblast/plugins/toggleterm.lua
@@ -0,0 +1,8 @@
+return {
+  'akinsho/toggleterm.nvim',
+  version = "*",
+  keys = {
+    { "<leader>oo", "<cmd>ToggleTerm direction=float<CR>", desc = "Toggle terminal window"},
+  },
+  config = true
+}
