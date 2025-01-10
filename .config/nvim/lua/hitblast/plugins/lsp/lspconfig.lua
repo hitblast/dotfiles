4afb7766228274c989058acfa5053c752351ae90
HitBlast <hitblastlive@gmail.com>
2025-01-10 16:00:48 +0600

    added basic configuration for python

--- .config/nvim/lua/hitblast/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/hitblast/plugins/lsp/lspconfig.lua
@@ -82,7 +82,6 @@ return {
         })
       end,
       ["lua_ls"] = function()
-        -- configure lua server (with special settings)
         lspconfig["lua_ls"].setup({
           capabilities = capabilities,
           settings = {
@@ -96,6 +95,36 @@ return {
               },
             },
           },
+        })
+      end,
+      ["pyright"] = function()
+        vim.api.nvim_create_autocmd("LspAttach", {
+          group = vim.api.nvim_create_augroup('lsp_attach_disable_ruff_hover', { clear = true }),
+          callback = function(args)
+            local client = vim.lsp.get_client_by_id(args.data.client_id)
+            if client == nil then
+              return
+            end
+            if client.name == 'ruff' then
+              -- Disable hover in favor of Pyright
+              client.server_capabilities.hoverProvider = false
+            end
+          end,
+          desc = 'LSP: Disable hover capability from Ruff',
+        })
+
+        lspconfig["pyright"].setup({
+          capabilities = capabilities,
+          settings = {
+            pyright = {
+              disableOrganizeImports = true,
+            },
+            python = {
+              analysis = {
+                ignore = { '*' }, -- Using Ruff
+              },
+            },
+          }
         })
       end,
     })
