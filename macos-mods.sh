a5f78b604c61761975f82b7fb6d5ec846e550c68
HitBlast <hitblastlive@gmail.com>
2025-02-02 17:18:45 +0600

    modified macOS settings script

--- macos-mods.sh
+++ macos-mods.sh
@@ -1,17 +1,15 @@
-read -p "macOS settings [ch (change) / revert (re)]:" user_input
+read -p "macOS settings [c (change) / revert (r)]:" user_input
 
-if [ "$user_input" == "ch" ]; then
+if [ "$user_input" == "c" ]; then
     # Menu bar clock modifications
     defaults write com.apple.menuextra.clock "FlashDateSeparators" -bool "true"
     defaults write com.apple.menuextra.clock "DateFormat" -string "\"HH:mm:ss\""
-    killall SystemUIServer
 
     # Finder modifications
     defaults write com.apple.finder "AppleShowAllFiles" -bool "true"
     defaults write com.apple.finder "CreateDesktop" -bool "false"
     defaults write com.apple.finder "ShowPathbar" -bool "true"
     defaults write com.apple.finder "FXRemoveOldTrashItems" -bool "true"
-    killall Finder
 
     # Trackpad modifications
     defaults write com.apple.AppleMultitouchTrackpad "FirstClickThreshold" -int "0"
@@ -24,20 +22,17 @@ if [ "$user_input" == "ch" ]; then
     defaults write com.apple.dock autohide-delay -float 0
     defaults write com.apple.dock autohide-time-modifier -float 0.6
     defaults write com.apple.dock "expose-group-apps" -bool "true"
-    killall Dock
 
-elif [ "$user_input" == "re" ]; then
+elif [ "$user_input" == "r" ]; then
     # Revert menu bar clock modifications
     defaults delete com.apple.menuextra.clock "FlashDateSeparators"
     defaults delete com.apple.menuextra.clock "DateFormat"
-    killall SystemUIServer
 
     # Revert Finder modifications
     defaults delete com.apple.finder "AppleShowAllFiles"
     defaults delete com.apple.finder "CreateDesktop"
     defaults delete com.apple.finder "ShowPathbar"
     defaults delete com.apple.finder "FXRemoveOldTrashItems"
-    killall Finder
 
     # Revert trackpad modifications
     defaults delete com.apple.AppleMultitouchTrackpad "FirstClickThreshold"
@@ -50,8 +45,13 @@ elif [ "$user_input" == "re" ]; then
     defaults delete com.apple.dock autohide-delay
     defaults delete com.apple.dock autohide-time-modifier
     defaults delete com.apple.dock "expose-group-apps"
-    killall Dock
 
 else
     echo "Invalid input."
 fi
+
+
+# This block ensures that the jobs are only killed after certain modifications and not each time the values are modified.
+killall SystemUIServer
+killall Finder
+killall Dock
