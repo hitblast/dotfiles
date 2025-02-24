90d23f896518fe061796ba6f2e929ef76ff7f542
HitBlast <hitblastlive@gmail.com>
2025-02-24 14:41:04 +0600

    updated plugin details

--- .config/nvim/lua/hitblast/plugins/markdown.lua
+++ .config/nvim/lua/hitblast/plugins/markdown.lua
@@ -1,10 +1,7 @@
 return {
-  'MeanderingProgrammer/markdown.nvim',
-  main = 'render-markdown',
-  opts = {},
-  name = 'render-markdown',
-  dependencies = {
-    'nvim-treesitter/nvim-treesitter',
-    'nvim-tree/nvim-web-devicons',
-  },
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
 }
