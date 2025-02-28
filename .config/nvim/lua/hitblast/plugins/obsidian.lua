502d16452c88e02a211ba951590ae596eb98cd27
HitBlast <hitblastlive@gmail.com>
2025-02-28 11:53:31 +0600

    updated configuration for obsidian

---
+++ .config/nvim/lua/hitblast/plugins/obsidian.lua
@@ -0,0 +1,24 @@
+return {
+  "epwalsh/obsidian.nvim",
+  version = "*",  -- recommended, use latest release instead of latest commit
+  lazy = true,
+  ft = "markdown",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  config = function()
+    local obsidian = require('obsidian')
+
+    obsidian.setup({
+      workspaces = {
+        {
+          name = "fogmoor",
+          path = "~/Documents/fogmoor"
+        },
+      },
+      ui = {
+        enable = false
+      },
+    })
+  end,
+}
