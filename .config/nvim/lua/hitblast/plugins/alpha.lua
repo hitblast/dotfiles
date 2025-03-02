ce433607490178a11975020bb530b35eee600bba
HitBlast <hitblastlive@gmail.com>
2025-03-02 09:10:52 +0600

    changed alpha screen

--- .config/nvim/lua/hitblast/plugins/alpha.lua
+++ .config/nvim/lua/hitblast/plugins/alpha.lua
@@ -1,32 +1,40 @@
 return {
-  "goolord/alpha-nvim",
-  event = "VimEnter",
-  config = function()
-    local alpha = require("alpha")
-    local dashboard = require("alpha.themes.dashboard")
+    "goolord/alpha-nvim",
+    event = "VimEnter",
+    config = function()
+        local alpha = require("alpha")
+        local dashboard = require("alpha.themes.dashboard")
 
-    -- header for dashboard
-    dashboard.section.header.val = {
-      "",
-      "",
-      "",
-      "",
-      "don't forget to run `brew update`",
-      "",
-    }
+        -- header for dashboard
+        dashboard.section.header.val = {
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "──▄▀▀▀▄───────────────",
+            "──█───█───────────────",
+            "─███████─────────▄▀▀▄─",
+            "░██─▀─██░░█▀█▀▀▀▀█░░█░",
+            "░███▄███░░▀░▀░░░░░▀▀░░",
+            "",
+        }
 
-    -- set menu
-    dashboard.section.buttons.val = {
-      dashboard.button("e", "New file", "<cmd>ene<CR>"),
-      dashboard.button("SPACE ee", "Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
-      dashboard.button("SPACE ff", "Find file", "<cmd>Telescope find_files<CR>"),
-      dashboard.button("SPACE fs", "Find string", "<cmd>Telescope live_grep<CR>"),
-      dashboard.button("q", "Quit Neovim", "<cmd>qa<CR>"),
-    }
+        -- set menu
+        dashboard.section.buttons.val = {
+            dashboard.button("e", "New file", "<cmd>ene<CR>"),
+            dashboard.button("SPACE ee", "Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
+            dashboard.button("SPACE ff", "Find file", "<cmd>Telescope find_files<CR>"),
+            dashboard.button("SPACE fs", "Find string", "<cmd>Telescope live_grep<CR>"),
+            dashboard.button("SPACE ll", "Open lazy window", "<cmd>Lazy<CR>"),
+            dashboard.button("SPACE mm", "Open mason window", "<cmd>Mason<CR>"),
+            dashboard.button("q", "Quit Neovim", "<cmd>qa<CR>"),
+        }
 
-    alpha.setup(dashboard.opts)
+        alpha.setup(dashboard.opts)
 
-    -- disable folding on alpha buffer
-    vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
-  end,
+        -- disable folding on alpha buffer
+        vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
+    end,
 }
