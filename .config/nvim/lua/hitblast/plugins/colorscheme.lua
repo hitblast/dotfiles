6e906e54043af0fcc79ef35492300bc2bfef80d2
HitBlast <hitblastlive@gmail.com>
2025-03-04 18:37:12 +0600

    changed theme to good ol' gruvbox

--- .config/nvim/lua/hitblast/plugins/colorscheme.lua
+++ .config/nvim/lua/hitblast/plugins/colorscheme.lua
@@ -1,34 +1,15 @@
 return {
-  "rebelot/kanagawa.nvim",
-  priority = 1000,
-  config = function()
-    local kanagawa = require('kanagawa')
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    config = function()
+        local gruvbox = require('gruvbox')
 
-    kanagawa.setup({
-      compile = true,             -- enable compiling the colorscheme
-      undercurl = true,            -- enable undercurls
-      commentStyle = { italic = true },
-      functionStyle = {},
-      keywordStyle = { italic = true},
-      statementStyle = { bold = true },
-      typeStyle = {},
-      transparent = false,         -- do not set background color
-      dimInactive = true,         -- dim inactive window `:h hl-NormalNC`
-      terminalColors = true,       -- define vim.g.terminal_color_{0,17}
-      colors = {                   -- add/modify theme and palette colors
-          palette = {},
-          theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
-      },
-      overrides = function(_) -- add/modify highlights
-          return {}
-      end,
-      theme = "dragon",              -- Load "wave" theme when 'background' option is not set
-      background = {               -- map the value of 'background' option to a theme
-          dark = "dragon",           -- try "dragon" !
-          light = "lotus"
-      },
-    })
+        gruvbox.setup({
+            terminal_colors = true,
+            dim_inactive = true,
+            transparent_mode = false,
+        })
 
-    vim.cmd("colorscheme kanagawa")
-  end,
+        vim.cmd("colorscheme gruvbox")
+    end,
 }
