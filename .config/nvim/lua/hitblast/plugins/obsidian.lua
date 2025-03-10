f3859337d8b6a66aea015453c99c9d23d79898ff
HitBlast <hitblastlive@gmail.com>
2025-03-10 18:46:50 +0600

    q

--- .config/nvim/lua/hitblast/plugins/obsidian.lua
+++ .config/nvim/lua/hitblast/plugins/obsidian.lua
@@ -1,7 +1,6 @@
 return {
     "epwalsh/obsidian.nvim",
     version = "*", -- recommended, use latest release instead of latest commit
-    ft = "markdown",
     dependencies = {
         "nvim-lua/plenary.nvim",
         "hrsh7th/nvim-cmp",
