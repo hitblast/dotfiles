e985090f859f0b920838a3934d95bd5d7e74b5c0
HitBlast <hitblastlive@gmail.com>
2025-05-27 20:58:13 +0600

    updated configuration

--- .config/nvim/lua/hitblast/plugins/neo-tree.lua
+++ .config/nvim/lua/hitblast/plugins/neo-tree.lua
@@ -1,28 +1,28 @@
 return {
-    'nvim-neo-tree/neo-tree.nvim',
-    version = '*',
-    dependencies = {
-        'nvim-lua/plenary.nvim',
-        'nvim-tree/nvim-web-devicons',
-        'MunifTanjim/nui.nvim',
-    },
-    lazy = false,
-    keys = {
-        { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
-    },
-    opts = {
-        filesystem = {
-            filtered_items = {
-                visible = true,           -- Show hidden files
-                show_hidden_count = true, -- Optional: show count of hidden files
-                hide_dotfiles = false,    -- Do not hide dotfiles
-                hide_gitignored = false,  -- Do not hide gitignored files
-            },
-            window = {
-                mappings = {
-                    ['\\'] = 'close_window',
-                },
-            },
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons',
+    'MunifTanjim/nui.nvim',
+  },
+  lazy = false,
+  keys = {
+    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+  },
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true, -- Show hidden files
+        show_hidden_count = true, -- Optional: show count of hidden files
+        hide_dotfiles = false, -- Do not hide dotfiles
+        hide_gitignored = false, -- Do not hide gitignored files
+      },
+      window = {
+        mappings = {
+          ['\\'] = 'close_window',
         },
+      },
     },
+  },
 }
