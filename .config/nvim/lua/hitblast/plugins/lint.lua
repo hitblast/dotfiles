eb89b395f12d1b5d940210ef78cccf762126b99e
HitBlast <hitblastlive@gmail.com>
2025-05-27 15:26:24 +0600

    updated configuration for linter + formatter

--- .config/nvim/lua/hitblast/plugins/lint.lua
+++ .config/nvim/lua/hitblast/plugins/lint.lua
@@ -4,9 +4,7 @@ return {
     event = { 'BufReadPre', 'BufNewFile' },
     config = function()
       local lint = require 'lint'
-      lint.linters_by_ft = {
-        markdown = { 'markdownlint' },
-      }
+      lint.linters_by_ft = {}
       local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
       vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
         group = lint_augroup,
