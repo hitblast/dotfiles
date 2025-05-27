7d78db5875014a7b3002cff8b447e790167e4f12
HitBlast <hitblastlive@gmail.com>
2025-05-27 15:26:24 +0600

    updated configuration for linter + formatter

--- .config/nvim/lua/hitblast/plugins/debug.lua
+++ .config/nvim/lua/hitblast/plugins/debug.lua
@@ -1,93 +1,91 @@
 return {
-    'mfussenegger/nvim-dap',
-    dependencies = {
-        'rcarriga/nvim-dap-ui',
-        'nvim-neotest/nvim-nio',
-        'mason-org/mason.nvim',
-        'jay-babu/mason-nvim-dap.nvim',
+  'mfussenegger/nvim-dap',
+  dependencies = {
+    'rcarriga/nvim-dap-ui',
+    'nvim-neotest/nvim-nio',
+    'mason-org/mason.nvim',
+    'jay-babu/mason-nvim-dap.nvim',
+  },
+  keys = {
+    {
+      '<F5>',
+      function()
+        require('dap').continue()
+      end,
+      desc = 'Debug: Start/Continue',
     },
-    keys = {
-        {
-            '<F5>',
-            function()
-                require('dap').continue()
-            end,
-            desc = 'Debug: Start/Continue',
-        },
-        {
-            '<F1>',
-            function()
-                require('dap').step_into()
-            end,
-            desc = 'Debug: Step Into',
-        },
-        {
-            '<F2>',
-            function()
-                require('dap').step_over()
-            end,
-            desc = 'Debug: Step Over',
-        },
-        {
-            '<F3>',
-            function()
-                require('dap').step_out()
-            end,
-            desc = 'Debug: Step Out',
-        },
-        {
-            '<leader>b',
-            function()
-                require('dap').toggle_breakpoint()
-            end,
-            desc = 'Debug: Toggle Breakpoint',
-        },
-        {
-            '<leader>B',
-            function()
-                require('dap').set_breakpoint(vim.fn.input 'Breakpoint condition: ')
-            end,
-            desc = 'Debug: Set Breakpoint',
-        },
-        {
-            '<F7>',
-            function()
-                require('dapui').toggle()
-            end,
-            desc = 'Debug: See last session result.',
-        },
+    {
+      '<F1>',
+      function()
+        require('dap').step_into()
+      end,
+      desc = 'Debug: Step Into',
     },
-    config = function()
-        local dap = require 'dap'
-        local dapui = require 'dapui'
+    {
+      '<F2>',
+      function()
+        require('dap').step_over()
+      end,
+      desc = 'Debug: Step Over',
+    },
+    {
+      '<F3>',
+      function()
+        require('dap').step_out()
+      end,
+      desc = 'Debug: Step Out',
+    },
+    {
+      '<leader>b',
+      function()
+        require('dap').toggle_breakpoint()
+      end,
+      desc = 'Debug: Toggle Breakpoint',
+    },
+    {
+      '<leader>B',
+      function()
+        require('dap').set_breakpoint(vim.fn.input 'Breakpoint condition: ')
+      end,
+      desc = 'Debug: Set Breakpoint',
+    },
+    {
+      '<F7>',
+      function()
+        require('dapui').toggle()
+      end,
+      desc = 'Debug: See last session result.',
+    },
+  },
+  config = function()
+    local dap = require 'dap'
+    local dapui = require 'dapui'
 
-        require('mason-nvim-dap').setup {
-            automatic_installation = true,
-            handlers = {},
-            ensure_installed = {
-                'delve',
-            },
-        }
+    require('mason-nvim-dap').setup {
+      automatic_installation = true,
+      handlers = {},
+      ensure_installed = {},
+    }
 
-        dapui.setup {
-            icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
-            controls = {
-                icons = {
-                    pause = '⏸',
-                    play = '▶',
-                    step_into = '⏎',
-                    step_over = '⏭',
-                    step_out = '⏮',
-                    step_back = 'b',
-                    run_last = '▶▶',
-                    terminate = '⏹',
-                    disconnect = '⏏',
-                },
-            },
-        }
+    dapui.setup {
+      icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
+      controls = {
+        icons = {
+          pause = '⏸',
+          play = '▶',
+          step_into = '⏎',
+          step_over = '⏭',
+          step_out = '⏮',
+          step_back = 'b',
+          run_last = '▶▶',
+          terminate = '⏹',
+          disconnect = '⏏',
+        },
+      },
+    }
 
-        dap.listeners.after.event_initialized['dapui_config'] = dapui.open
-        dap.listeners.before.event_terminated['dapui_config'] = dapui.close
-        dap.listeners.before.event_exited['dapui_config'] = dapui.close
-    end,
+    dap.listeners.after.event_initialized['dapui_config'] = dapui.open
+    dap.listeners.before.event_terminated['dapui_config'] = dapui.close
+    dap.listeners.before.event_exited['dapui_config'] = dapui.close
+  end,
 }
