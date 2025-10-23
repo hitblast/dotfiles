9a4f0eef7a7bd4251a57a37330b195dcff6e78e7
Anindya Shiddhartha <hitblastlive@gmail.com>
2025-10-23 18:37:14 +0600

    update configurations

--- .config/nvim/lua/config/plugins/kanagawa.lua
+++ .config/nvim/lua/config/plugins/kanagawa.lua
@@ -1,8 +1,6 @@
 return {
     "rebelot/kanagawa.nvim",
     config = function()
-        -- Detect system theme using vim.o.background
-        local variant = (vim.o.background == "light") and "kanagawa-lotus" or "kanagawa-wave"
-        vim.cmd.colorscheme(variant)
+        vim.cmd.colorscheme("kanagawa-dragon")
     end,
 }
