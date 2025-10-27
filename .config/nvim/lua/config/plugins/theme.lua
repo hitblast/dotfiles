aac828fc89dce3edb97adfcbabfae7d76dbfd77c
Anindya Shiddhartha <hitblastlive@gmail.com>
2025-10-27 19:11:07 +0600

    fix: theme

--- .config/nvim/lua/config/plugins/theme.lua
+++ .config/nvim/lua/config/plugins/theme.lua
@@ -1,9 +1,9 @@
 return {
-  "idr4n/github-monochrome.nvim",
+  "aktersnurra/no-clown-fiesta.nvim",
   lazy = false,
   priority = 1000,
   opts = {},
   config = function()
-	  vim.cmd("colorscheme github-monochrome-light")
+	  vim.cmd("colorscheme no-clown-fiesta")
   end,
 }
