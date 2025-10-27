8e297cbfc64a0a6f5857a0501310c32e72733e48
Anindya Shiddhartha <hitblastlive@gmail.com>
2025-10-27 15:40:43 +0600

    feat: de-facto commit

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,1 +1,10 @@
+-- vim opts
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- useful for completions
+-- this makes it easy to understand
+vim.opt.completeopt = { "menuone", "noselect", "popup" }
+
+-- lazy setup
 require("config.lazy")
--- .config/nvim/lua/config/plugins/lsp-config.lua
+++ .config/nvim/lua/config/plugins/lsp-config.lua
@@ -1,26 +1,42 @@
 return {
-  {
-    'mason-org/mason.nvim',
-    opts = {},
-  },
-  {
-    "mason-org/mason-lspconfig.nvim",
-    opts = {
-      ensure_installed = {
-        "lua_ls",
-        "basedpyright",
-        "rust_analyzer",
-      },
+    {
+        "mason-org/mason.nvim",
+        opts = {},
     },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-        local lspconfig = vim.lsp
+    {
+        "mason-org/mason-lspconfig.nvim",
+        opts = {
+            ensure_installed = { "lua_ls" },
+        },
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            vim.api.nvim_create_autocmd("LspAttach", {
+                callback = function(args)
+                    local client = vim.lsp.get_client_by_id(args.data.client_id)
+                    local bufnr = args.buf
 
-        lspconfig.enable("lua_ls")
-        lspconfig.enable("basedpyright")
-        lspconfig.enable("rust_analyzer")
-    end
-  },
+                    vim.lsp.completion.enable(true, client.id, bufnr, {
+                        autotrigger = true,
+                        convert = function(item)
+                            return { abbr = item.label:gsub("%b()", "") }
+                        end,
+                    })
+                end,
+            })
+
+            local servers = {
+                rust_analyzer = {
+                    cmd = { vim.fn.expand("~/.config/cargo/bin/rust-analyzer") }, -- custom binary
+                },
+                lua_ls = {},
+            }
+
+            for name, config in pairs(servers) do
+                vim.lsp.config[name] = config
+                vim.lsp.enable(name)
+            end
+        end,
+    },
 }
